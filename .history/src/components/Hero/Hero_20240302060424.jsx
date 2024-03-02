5bbb5df8b1d23798eec66d0de2f854d4caaebe2a
ARUN YERRAM <arunyerram12022005@gail.com>
2024-03-02 06:32:18 +0530

    first commit

---
+++ .history/src/components/Hero/Hero_20240302060424.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Arun Yerram</h1>
+        <p className={styles.description}>
+          {/* I'm a full-stack developer with 5 years of experience using React and */}
+          {/* NodeJS. Reach out if you'd like to learn more! */}
+          I am Pursing my Bachelor's 
+
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
