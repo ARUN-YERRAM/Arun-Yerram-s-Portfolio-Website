25f9587e50ed9061b4a4c22f099fc33c7848913a
ARUN YERRAM <arunyerram12022005@gail.com>
2024-07-01 22:22:55 +0530

    Update commits

---
+++ .history/src/components/CodingProfiles/CodingProfiles_20240625215056.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./CodingProfiles.module.css";
+import profiles from "../../data/profiles.json";
+
+export const CodingProfiles = () => {
+  return (
+    <section className={styles.container} id="coding-profiles">
+      <h2 className={styles.title}>Coding Profiles</h2>
+      <div className={styles.content}>
+        {profiles.map((profile, id) => (
+          <div key={id} className={styles.profile}>
+            <div className={styles.profileImageContainer}>
+              <img
+                src={"..assets/codingprofiles/Leetcode.png"}
+                alt={`${profile.username}`}
+                className={styles.avatar}
+              />
+            </div>
+            <div className={styles.profileDetails}>
+              <h3>{profile.username}</h3>
+              <p>{profile.platform}</p>
+              <a href={profile.profileUrl} target="_blank" rel="noopener noreferrer">
+                View Profile
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
---
+++ .history/src/components/Hero/Hero_20240625211047.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Arun Yerram</h1>
+        <p className={styles.description}>
+          {/* I'm a full-stack developer with 5 years of experience using React and */}
+          {/* NodeJS. Reach out if you'd like to learn more! */}
+          I am Pursing my Bachelor's 2nd year and having keen interest in Problem Solving ,Web Development, and Data Science.
+
+        </p>
+        <a href="mailto:arunyerram12022005@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/Passport1.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+        width="50px"
+        height="80"
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
