2d8c2c1c702f27cb3ef1c118311521984f64728a
ARUN YERRAM <arunyerram12022005@gail.com>
2024-07-07 00:36:26 +0530

    Arun commits

---
+++ .history/docs/components/CodingProfiles/CodingProfiles_20240706235912.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./CodingProfiles.module.css";
+import profiles from "../../data/codingprofiles.json";
+
+export const CodingProfiles = () => {
+  return (
+    <section className={`${styles.container} ${styles.animatedBackground}`}>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Coding Profiles</h2>
+        <p className={styles.description}>
+          Check out my coding profiles on various platforms!
+        </p>
+      </div>
+      <ul className={styles.links}>
+        {profiles.map((profile, id) => (
+          <li key={id} className={styles.link}>
+            <a href={profile.url} target="_blank" rel="noopener noreferrer">
+              {profile.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+// export default CodingProfiles;
---
+++ .history/docs/src/App_20240707002847.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import styles from './App.module.css';
+import { About } from '../components/About/About';
+import { CodingProfiles } from '../components/CodingProfiles/CodingProfiles';
+import { Contact } from '../components/Contact/Contact';
+import { Experience } from '../components/Experience/Experience';
+import { Hero } from '../components/Hero/Hero';
+import { Navbar } from '../components/Navbar/Navbar';
+import { Projects } from '../components/Projects/Projects';
+
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.body.classList.toggle('dark-mode'); // Toggle dark-mode class on body
+  };
+
+  return (
+    <div className={`${styles.App} ${darkMode ? styles.darkMode : ''}`}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <CodingProfiles />
+      <Contact />
+      <button className={styles.darkModeToggle} onClick={toggleDarkMode}>
+        {darkMode ? 'Light Mode' : 'Dark Mode'}
+      </button>
+    </div>
+  );
+};
+
+export default App;
